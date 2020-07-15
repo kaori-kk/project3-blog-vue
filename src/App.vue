1d7e3bd7aa6d0ce461ba8760517b27b197f4aec9
kaori kohama <kkaori131415@gmail.com>
2020-07-15 14:46:51 +0200

    created navbar using vue router!

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">Signup</router-link> 
-    </div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+* {
+  font-family: 'Poppins', sans-serif;
+  border-radius: 0;
 }
 
-#nav {
-  padding: 30px;
+body {
+  background-color: #f5f3ef;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  
 </style>
