0db212ca95a7c3e4b6442ebbfb96ea641ad86364
kaori kohama <kkaori131415@gmail.com>
2020-07-15 15:44:24 +0200

    added form to login/signup page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="signup">
-      <h1>signup component</h1>
+  <div class="row my-5">
+    <div class="col-md-8 offset-md-2">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Signup</h3>
+          <div class="form-group">
+            <input type="text" placeholder="Name" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button class="btn btn-info form-control">Signup</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
