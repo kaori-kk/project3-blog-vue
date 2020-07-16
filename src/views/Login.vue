78078d48b9ab192e5c52a0237fdf25001192be66
kaori kohama <kkaori131415@gmail.com>
2020-07-16 15:24:47 +0200

    displaying errors in login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,16 +5,48 @@
         <div class="card-body">
           <h3 class="text-center my-4">Login</h3>
           <div class="form-group">
-            <input type="text" placeholder="Email" class="form-control">
+            <input v-model="email" type="text" placeholder="Email" class="form-control">
           </div>
           <div class="form-group">
-            <input type="password" placeholder="Password" class="form-control">
+            <input v-model="password" type="password" placeholder="Password" class="form-control">
+            <div class="errors" v-if="errors.email">
+              <small class="text-danger" v-for="error in errors.email" :key="error">{{error}}</small>
+            </div>
           </div>
           <div class="form-group text-center">
-            <button class="btn btn-info form-control">Login</button>
+            <button @click="login" class="btn btn-info form-control">Login</button>
+            <div class="errors" v-if="errors.password">
+              <small class="text-danger" v-for="error in errors.password" :key="error">{{error}}</small>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  data(){
+    return {
+      email: "",
+      password: "",
+      errors: {}
+    }
+  },
+  methods: {
+    login(){
+      Axios.post("https://react-blog-api.bahdcasts.com/api/auth/login", {
+        email: this.email,
+        password: this.password
+      }).then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        this.errors = response.data;
+      })
+    }
+  }
+}
+</script>
